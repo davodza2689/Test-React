957aedadec3f5d59b4695f778886cde3491c7b90
davodza2689 <davidharutyunyan2689@gmail.com>
2018-02-19 17:05:49 +0100

    Create base structure

---
+++ src/new.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+export default class NewComponent extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    renderItem(item, idx) {
+        return (
+            <li key={idx}><b>{item.text}</b> - {item.id}</li>
+        );
+    }
+
+    render() {
+        return (
+            <ul>
+                { this.props.array.map(this.renderItem.bind(this)) }
+            </ul>
+        );
+    }
+}
+
